76c98eb02ae3d2b212cc83640c47102424688470
admin <admin@almogtama3.com>
2018-12-13 14:26:27 +0300

    convert -> write data -> create tmp
    added: creatre temporary collections
    added: await for writing data before creating tmp collections

--- db/_install.js
+++ db/_install.js
@@ -1,88 +1,92 @@
 /*
 nx: use insertMany()
+nx: call articles_update.js
+use import '@babel/polyfill' when a file contains async/await to avoid 'regeneratorRuntime is not defined' when transpile this file with babel (must be @first line)
 */
+import '@babel/polyfill'
+import update from './articles_update.js'
+import dbx from './index.js'
+import fs from 'fs'
+import convert from './_convert.js'
 
-const convert = require('./_convert.js'),
-  dbx = require('./index.js'),
-  fs = require('fs')
 convert
-  .done(() => {
-    dbx
-      .connect()
-      .done(db => {
-        console.log('========= connected ============')
-        //console.log('db:', db)
-        let dir = './__db/step4',
-          models = [],
-          files = fs.readdirSync(dir)
+  .then(() => dbx.connect())
+  .then(async db => {
+    console.log('========= connected ============')
+    //console.log('db:', db)
+    let dir = './__db/step4',
+      models = [],
+      files = fs.readdirSync(dir)
 
-        for (let f = 0; f < files.length; f++) {
-          let file = files[f]
-          if (file.slice(-5) != '.json') continue
-          let coll = file.slice(0, -5),
-            data = require(`${dir}/${file}`),
-            obj = require(`./schema/${coll}.js`), //contains schema & indexes
-            { model } = db.model(coll, obj[0]) //true: add insertObj()
-          models[coll] = model //to avoid re compile the model again (mongoose error)
-          //test validation
-          console.log(`validate coll: ${coll} =========`)
-          for (let i = 0; i < data.length; i++) {
-            let doc = new model(data[i]),
-              invalid = doc.validateSync()
-            if (invalid)
-              throw {
-                //or Promise.reject()
-                msg: 'validation error',
-                error: invalid,
-                doc: doc
-              }
-            //if any collection has validation error, don't insert any data ar create any index
-            else console.log(`validate ${coll}-${i}: ok`)
+    for (let f = 0; f < files.length; f++) {
+      let file = files[f]
+      if (file.slice(-5) != '.json') continue
+      let coll = file.slice(0, -5),
+        data = require(`${dir}/${file}`),
+        obj = require(`./schema/${coll}.js`), //contains schema & indexes
+        { model } = db.model(coll, obj[0]) //true: add insertObj()
+      models[coll] = model //to avoid re compile the model again (mongoose error)
+      //test validation
+      console.log(`validate coll: ${coll} =========`)
+      for (let i = 0; i < data.length; i++) {
+        let doc = new model(data[i]),
+          invalid = doc.validateSync()
+        if (invalid)
+          throw {
+            //or Promise.reject()
+            msg: 'validation error',
+            error: invalid,
+            doc: doc
           }
-        }
-        console.log('========= validation Done ============')
+        //if any collection has validation error, don't insert any data ar create any index
+        else console.log(`validate ${coll}-${i}: ok`)
+      }
+    }
+    console.log('========= validation Done ============')
 
-        //process.exit()
-        for (let f = 0; f < files.length; f++) {
-          let file = files[f]
-          if (file.slice(-5) != '.json') continue
-          let coll = file.slice(0, -5),
-            data = require(`${dir}/${file}`),
-            obj = require(`./schema/${coll}.js`),
-            model = models[coll]
-          //create indexes
-          for (let i = 1; i < obj.length; i++) {
-            db.index(model, obj[i]).then(
-              index => console.log(`${coll} index-${i} created`, index),
-              err => {
-                throw {
-                  msg: `${coll} index-${i} error:`,
-                  error: err
-                }
-              }
-            )
+    //process.exit()
+    for (let f = 0; f < files.length; f++) {
+      let file = files[f]
+      if (file.slice(-5) != '.json') continue
+      let coll = file.slice(0, -5),
+        data = require(`${dir}/${file}`),
+        obj = require(`./schema/${coll}.js`),
+        model = models[coll]
+      //create indexes
+      for (let i = 1; i < obj.length; i++) {
+        await db.index(model, obj[i]).then(
+          index => console.log(`${coll} index-${i} created`, index),
+          err => {
+            throw {
+              msg: `${coll} index-${i} error:`,
+              error: err
+            }
           }
+        )
+      }
 
-          //insert data
-          for (let i = 0; i < data.length; i++) {
-            model.create(data[i]).then(
-              doc => console.log(`${coll} data-${i}: OK`),
-              err => {
-                throw {
-                  msg: `insert: ${coll} data-${i}`,
-                  error: err
-                }
-              }
-            )
+      //insert data
+      for (let i = 0; i < data.length; i++) {
+        await model.create(data[i]).then(
+          doc => console.log(`${coll} data-${i}: OK`),
+          err => {
+            throw {
+              msg: `insert: ${coll} data-${i}`,
+              error: err
+            }
           }
-        }
+        )
+      }
+    }
 
-        console.log('========= DONE ============')
-      })
-      .fail(err => console.log('error#1:', err))
+    return { db, models } //must wait untin creating indexes & writing data finished
   })
-
-  .fail(err => console.log('error#2:', err))
+  .then(({ db, models }) => {
+    //create tmp collections
+    update(db, models['articles'])
+    console.log('========= DONE ============')
+  })
+  .fail(err => console.log('error:', err))
 
 /*db.done(db => {
   content = require(`./articles.js`)
